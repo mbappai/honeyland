fb2906a5674d5e8babdef98d1b226eb7da9f45be
mujeex <mujahid.bappai@yahoo.com>
2022-06-05 10:13:34 +0100

    refactor: use openzeppelin erc721 lib instead of boilerplate

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -1,4 +1,5 @@
-pragma solidity 0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
 // TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
 

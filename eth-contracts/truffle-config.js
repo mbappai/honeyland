d92b5c195191708a328dda068d48c36de1d1cd0e
mujeex <mujahid.bappai@yahoo.com>
2022-06-15 19:23:16 +0100

    chore: install hdwallet and dotenv package

--- eth-contracts/truffle-config.js
+++ eth-contracts/truffle-config.js
@@ -18,11 +18,11 @@
  *
  */
 
-// const HDWalletProvider = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+require('dotenv').config();
+
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
 
 module.exports = {
   /**
@@ -60,14 +60,18 @@ module.exports = {
 
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-      // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/${infuraKey}`),
-      // network_id: 3,       // Ropsten's id
-      // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-      // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    rinkeby: {
+      provider: () => new HDWalletProvider( process.env.MNEMONIC_PHRASE,
+         `https://rinkeby.infura.io/v3/${process.env.INFURA_KEY}`),
+        // numberOfAddresses: 1,
+        // from:'0x87906b0E252B370251E58730D7B88FdEEC9d9d8e',
+        network_id:4,
+        shareNonce: true,
+        derivationPath: "m/44'/1'/0'/0/",
+        networkCheckTimeoutnetworkCheckTimeout: 10000,
+        timeoutBlocks: 200
+      ,
+    },
 
     // Useful for private networks
     // private: {
